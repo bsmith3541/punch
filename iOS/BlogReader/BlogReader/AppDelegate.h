3cc273a188f46b9a4dafd2c42172eb732f5e3d69
Brandon Smith <brandon.smith@yale.edu>
2013-03-15 09:15:22 -0400

    added TableViewController

---
+++ iOS/BlogReader/BlogReader/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BlogReader
+//
+//  Created by Brandon Smith on 3/14/13.
+//  Copyright (c) 2013 Brandon Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS/BlogReader/BlogReader/TableViewController.h
@@ -0,0 +1,15 @@
+//
+//  TableViewController.h
+//  BlogReader
+//
+//  Created by Brandon Smith on 3/14/13.
+//  Copyright (c) 2013 Brandon Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@property (nonatomic, strong) NSArray *titles;
+
+@end
